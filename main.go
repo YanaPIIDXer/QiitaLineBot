bf98006f670de2034bc6e611a22f72c13028ca1d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-01 09:37:23 +0900

    LINEからのリクエストを受け付ける箇所を作成。 ※文字列返すだけ。

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 func main() {
     router := gin.Default()
 
+	// ローカルからの接続
     router.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "HTTP SERVER")
-    })
+	})
+	
+	// LINEからのリクエスト
+	router.POST("/line_client", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Line Client")
+	})
 
     router.Run(":3000")
 }
