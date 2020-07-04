3496b995dffffe43052544615010a20009b250be
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-04 14:03:04 +0900

    mainで動かしていたオウム返し処理をLINEConnectionクラスに移植。 それに伴いmainの馬鹿デカいコメントアウトを撤去。

--- main.go
+++ main.go
@@ -10,67 +10,13 @@ import (
 func main() {
 	godotenv.Load(".env")
 
-	var port = os.Getenv("PORT")		// heroku上で動かす場合は指定されたPortじゃないと死なますよ。
-	if port == "" {
-		port = "3000"
-	}
-
 	var connection = InfrastructureConnection.NewHTTPConnection()
 	connection.AddAccessEvent(InfrastructureConnection.NewLocalAccessEvent())
 	connection.AddAccessEvent(InfrastructureConnection.NewLINEAccessEvent())
-	connection.Service(port)
 	
-	/*
-	// この辺の処理、mainに直接書くのもなぁ・・・
-	// @TODO:後程クラス化。
-	router := gin.Default()
-	
-	var channelSecret = os.Getenv("CHANNEL_SECRET")
-	var channelToken = os.Getenv("CHANNEL_TOKEN")
-	bot, err := linebot.New(channelSecret, channelToken)
-	if err != nil {
-		fmt.Println("Line Bot Instantiate Failed.")
-		fmt.Println("Channel Secret:" + channelSecret)
-		fmt.Println("Channel Token:" + channelToken)
-		return
-	}
-
-	// ローカルからの接続
-    router.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "LOCAL SERVER")
-	})
-	
-	// LINEからのリクエスト
-	router.POST("/line_client", func(ctx *gin.Context) {
-		events, err := bot.ParseRequest(ctx.Request)
-		if err != nil {
-			fmt.Println("Parse Request Failed.")
-			if err == linebot.ErrInvalidSignature {
-				fmt.Println(" ...Invalid Signature.")
-			}
-			return
-		}
-
-		for _, event := range events {
-			switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					// ↓ReplyMessageメソッドが返すのはエラーオブジェクトではなくReplyMessageCallと言うオブジェクトで、
-					//  ここでメッセージが送信されるわけではない。
-					//  そいつのDoメソッドを叩くことで初めてメッセージが送信される。
-					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
-					if err != nil {
-						fmt.Println("ReplyMessage Failed.")
-					}
-					break
-			}
-		}
-	})
-
 	var port = os.Getenv("PORT")		// heroku上で動かす場合は指定されたPortじゃないと死なますよ。
 	if port == "" {
 		port = "3000"
 	}
-	fmt.Println("Server Port:" + port)
-	router.Run(":" + port)
-	*/
+	connection.Service(port)
 }
--- src/infrastructure/connection/LINEConnection.go
+++ src/infrastructure/connection/LINEConnection.go
@@ -1,19 +1,34 @@
 package InfrastructureConnection
 
 import (
-	"net/http"
+	"fmt"
+	"os"
 	"github.com/gin-gonic/gin"
-//	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/linebot"
 
 	"github.com/YanaPIIDXer/QiitaLineBot/src/domain/connection"
 )
 
 // LINEからの接続
-type LINEAccessEvent struct {}
+type LINEAccessEvent struct {
+	bot *linebot.Client		// BOTインスタンス
+}
 
 // コンストラクタ
 func NewLINEAccessEvent() *LINEAccessEvent {
 	var accessEvent = new(LINEAccessEvent)
+	
+	var channelSecret = os.Getenv("CHANNEL_SECRET")
+	var channelToken = os.Getenv("CHANNEL_TOKEN")
+	bot, err := linebot.New(channelSecret, channelToken)
+	if err != nil {
+		fmt.Println("Line Bot Instantiate Failed.")
+		fmt.Println("Channel Secret:" + channelSecret)
+		fmt.Println("Channel Token:" + channelToken)
+		return nil
+	}
+	accessEvent.bot = bot
+
 	return accessEvent
 }
 
@@ -25,5 +40,27 @@ func (this *LINEAccessEvent) GetInfo() (Connection.AccessMethod, string) {
 // 接続された時に走るイベント
 func (this *LINEAccessEvent) OnAccess(gtx interface{}) {
 	context := gtx.(*gin.Context)
-	context.String(http.StatusOK, "LINE Access Event Running...")
+	events, err := this.bot.ParseRequest(context.Request)
+	if err != nil {
+		fmt.Println("Parse Request Failed.")
+		if err == linebot.ErrInvalidSignature {
+			fmt.Println(" ...Invalid Signature.")
+		}
+		return
+	}
+
+	for _, event := range events {
+		switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				// ↓ReplyMessageメソッドが返すのはエラーオブジェクトではなくReplyMessageCallと言うオブジェクトで、
+				//  ここでメッセージが送信されるわけではない。
+				//  そいつのDoメソッドを叩くことで初めてメッセージが送信される。
+				_, err = this.bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do()
+				if err != nil {
+					fmt.Println("ReplyMessage Failed.")
+				}
+				break
+		}
+	}
+
 }
