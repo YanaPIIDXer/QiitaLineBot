1d0742f169e733fc19d37637f412912532477011
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-04 12:56:11 +0900

    「HTTP」と言う単語を撤去。

--- src/domain/connection/ConnectionInterface.go
+++ src/domain/connection/ConnectionInterface.go
@@ -1,7 +1,7 @@
 package Connection
 
-// HTTP通信インタフェース
-type HTTPConnectionInterface interface {
+// 通信インタフェース
+type ConnectionInterface interface {
 	AddAccessEvent(*AccessEventInterface)		// アクセスイベント追加
 	Service(string)		// 実行開始
 }
